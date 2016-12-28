24df1551cb359b262a34d0dccfee1db60101f3b7
StoychoPetrov <StoychoPetrov95@gmail.com>
2016-12-28 16:38:38 +0200

    changes

--- app/src/main/java/com/example/stoycho/phonebook/adapters/UsersRecyclerAdapter.java
+++ app/src/main/java/com/example/stoycho/phonebook/adapters/UsersRecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.stoycho.phonebook.Interfaces.OnRecyclerItemClick;
